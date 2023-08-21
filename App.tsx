b6923944e5bc73b3ac83f6f55739e4df20aeb23f
Bappy71 <19103076@iubat.edu>
2023-08-21 16:36:00 +0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './SRC/Home/Home';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-  <NavigationContainer>
-  <Stack.Navigator screenOptions={{headerShown:false}}
-  <Stack.Screen name='Home' component={Home}></Stack.Screen>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
