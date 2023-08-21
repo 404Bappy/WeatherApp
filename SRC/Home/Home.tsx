4bfa9dcc71106b199167477dc51f7186479a8472
Bappy71 <19103076@iubat.edu>
2023-08-21 16:49:01 +0600

    first commit

--- SRC/Home/Home.tsx
+++ SRC/Home/Home.tsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
 import React from 'react';
 
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      <ImageBackground
+        source={require('../../Assets/pngtree-hd-cool-background-illustration-picture-image_1131674.jpeg')}></ImageBackground>
     </View>
   );
 }
