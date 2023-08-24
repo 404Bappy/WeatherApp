906dc9a9a427b611e718e01930b832b6e081ee12
Bappy71 <19103076@iubat.edu>
2023-08-24 13:04:59 +0600

    first commit

--- SRC/Home/Home.js
+++ SRC/Home/Home.js
@@ -1,4 +1,4 @@
-import {View, ImageBackground} from 'react-native';
+import {View, ImageBackground, Image, Text} from 'react-native';
 import React from 'react';
 import {deviceHeight, deviceWidth} from '../Dimentions/Dimention';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -11,10 +11,34 @@ export default function Home() {
         style={{height: deviceHeight, width: deviceWidth}}
         imageStyle={{opacity: 1, backgroundColor: 'black'}}
       />
-      <View style={{position: 'absolute'}}>
-        <View>
+      <View
+        style={{
+          position: 'absolute',
+          paddingVertical: 20,
+          paddingHorizontal: 10,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            width: deviceWidth - 20,
+          }}>
           <Icon name="menu" size={45} />
+          <Image
+            source={require('../../Assets/memex.png')}
+            style={{
+              height: 45,
+              width: 45,
+              borderRadius: 50,
+              borderColor: '#fff',
+            }}
+          />
         </View>
+        <Text style={{fontSize: 40, color: '#fff'}}>Hello Folks!</Text>
+        <Text style={{color: '#fff', fontSize: 22, fontWeight: 'bold'}}>
+          Search The City By Name
+        </Text>
       </View>
     </View>
   );
