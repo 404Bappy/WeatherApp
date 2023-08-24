a6cf03730b0e45141b05ec6fcf36bb084013f6a2
Bappy71 <19103076@iubat.edu>
2023-08-24 14:44:50 +0600

    first commit

--- SRC/Home/Home.js
+++ SRC/Home/Home.js
@@ -1,9 +1,17 @@
-import {View, ImageBackground, Image, Text, TextInput} from 'react-native';
-import React from 'react';
+import {
+  View,
+  ImageBackground,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
 import {deviceHeight, deviceWidth} from '../Dimentions/Dimention';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function Home() {
+  const [city, setCity] = useState(['']);
   return (
     <View>
       <ImageBackground
@@ -35,24 +43,44 @@ export default function Home() {
             }}
           />
         </View>
-        <View style={{paddingHorizontal: 20, marginTop: 100}}>
-          <Text style={{fontSize: 40, color: 'darkblack'}}>Hello Folks!</Text>
-          <Text style={{color: 'darkblack', fontSize: 22, fontWeight: 'bold'}}>
+        <View style={{paddingHorizontal: 10, marginTop: 100}}>
+          <Text style={{fontSize: 40, color: 'darkblack', textAlign: 'center'}}>
+            Hello Folks!
+          </Text>
+          <Text
+            style={{
+              color: 'darkblack',
+              fontSize: 22,
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}>
             Search The City By Name
           </Text>
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            borderWidth: 1,
+            borderRadius: 10,
+            paddingHorizontal: 10,
+            marginTop: 10,
+          }}>
           <TextInput
+            value={city}
+            onChangeText={val => setCity(val)}
             placeholder="Search city"
-            style={{paddingHorizontal: 10}}></TextInput>
+            style={{paddingHorizontal: 10, fontSize: 16}}></TextInput>
 
-          <Icon
-            name={'search'}
-            size={22}
-            color={'black'}
-            style={{paddingVertical: 15}}
-          />
+          <TouchableOpacity onPress={() => {}}>
+            <Icon
+              name={'search'}
+              size={22}
+              style={{paddingVertical: 15, paddingHorizontal: 10}}
+            />
+          </TouchableOpacity>
         </View>
+        <Text style={{fontSize: 22, paddingHorizontal: 10}}>My Location</Text>
       </View>
     </View>
   );
